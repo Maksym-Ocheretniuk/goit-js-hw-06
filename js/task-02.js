e871884dfc013b638b524a9dff4512121f896ead
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-07 21:19:58 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// console.log(ingredients);
+
+const mainListEl = document.querySelector("#ingredients");
+// console.log(mainListEl);
+
+const ingredientsListEl = ingredients.map(ingredient => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredient;
+  listEl.classList.add("item");
+  // console.log(listEl);
+
+  return listEl;
+});
+
+const allEl = ingredients => ingredientsListEl;
+
+const elements = allEl(ingredients);
+
+mainListEl.append(...elements);

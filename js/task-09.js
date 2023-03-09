b0613ed2870a480ed21ccfe431d1f56d2e0d223b
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-10 01:19:37 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+const bodyEl = document.querySelector("body");
+const changeColorBtnEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+
+changeColorBtnEl.addEventListener("click", onChangeColorBody);
+
+function onChangeColorBody() {
+  /* альтернативний розпис коду */
+  // const newColor = getRandomHexColor();
+  // bodyEl.style.backgroundColor = newColor;
+  // textColorEl.textContent = newColor;
+
+  textColorEl.textContent = bodyEl.style.backgroundColor = getRandomHexColor();
+
+  // console.log("Body Color = ", bodyEl.style.backgroundColor);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)

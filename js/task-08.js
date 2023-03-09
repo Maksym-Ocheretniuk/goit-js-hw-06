c86367db9ed548b2cb7faae87aecaafe973d5120
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-09 20:43:56 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+console.dir(formEl.elements);
+
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mailEl = formElements.email.value;
+  const passwordEl = formElements.password.value;
+
+  if (!mailEl || !passwordEl) {
+    alert("Не всі поля заповнено! Будь ласка, заповніть всі поля форми!");
+  } else {
+    const formData = { mail: mailEl, password: passwordEl };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+  }
+}
